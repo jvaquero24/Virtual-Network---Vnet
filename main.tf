10c7ca6e0ca6993094592d4bb233bd7183a46c9c
jvaquero24 <67736596+jvaquero24@users.noreply.github.com>
2020-10-15 20:58:08 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "azurerm_virtual_network" "VNET" {
   name                = var.vnet_name
   location            = var.location
   resource_group_name = data.azurerm_resource_group.RG.name
-  address_space       = var.address_space
+  address_space       = ["10.0.0.0/16"]
 
   subnet {
     name           = var.name_subnet_1
