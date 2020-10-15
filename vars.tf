24e4b442bf7a0cac19286de0ab2ed7812ef1ac28
jvaquero24 <67736596+jvaquero24@users.noreply.github.com>
2020-10-15 13:33:57 +0200

    Create vars.tf

---
+++ vars.tf
@@ -0,0 +1,24 @@
+  variable "vnet_name" {
+  type        = string
+}
+variable "location" {
+  type        = string
+}
+variable "address_space" {
+  type        = string
+}
+variable "resource_group_name" {
+  type        = string
+}
+variable "name_subnet_1" {
+  type        = string
+}
+variable "name_subnet_2" {
+  type        = string
+}
+variable "IPsubnet_1" {
+  type        = string
+}
+variable "IPsubnet_2" {
+  type        = string
+}
